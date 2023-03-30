01604543b4700c2f4ec74720e606060b88dbb21c
Alain <alain.dossche@vdabcampus.be>
2023-03-30 15:07:29 +0200

    cursus p11 start oef LeerlingRepository

---
+++ src/main/java/be/vdab/welkom/exceptions/RepositoryException.java
@@ -0,0 +1,7 @@
+package be.vdab.welkom.exceptions;
+
+public class RepositoryException extends RuntimeException{
+    public RepositoryException(Exception cause) {
+        super(cause);
+    }
+}
--- src/main/java/be/vdab/welkom/repositories/LandRepository.java
+++ src/main/java/be/vdab/welkom/repositories/LandRepository.java
@@ -1,10 +1,30 @@
 package be.vdab.welkom.repositories;
 
+import be.vdab.welkom.domain.Land;
+import be.vdab.welkom.exceptions.RepositoryException;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Component
 public class LandRepository {
-    public LandRepository() {
-        System.out.println("LandRepository constructor");
+    public List<Land> findAll() {
+        try (var stream = Files.lines(Path.of("/data/landen.csv"))) {
+            return stream
+                    .map(regel -> regel.split(",")) // regel splitsen in zijn onderdelen
+                    .map(regelOnderdelen -> // Land object maken met die onderdelen
+                            new Land(regelOnderdelen[0], // landcode
+                                    regelOnderdelen[1], // naam
+                                    Integer.parseInt(regelOnderdelen[2]))) // oppervlakte
+                    .toList(); // een List maken op basis van de stream
+        } catch (IOException | ArrayIndexOutOfBoundsException |
+                 NumberFormatException ex) {
+            throw new RepositoryException(ex);
+        }
     }
-}
+    }
+
+
