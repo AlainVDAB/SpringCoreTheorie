33470e11c0f3b47f6fdba3498645b6ac4b6e3ecb
Alain <alain.dossche@vdabcampus.be>
2023-03-30 14:11:05 +0200

    cursus p9 start oef Bean

---
+++ src/main/java/be/vdab/welkom/repositories/LandRepository.java
@@ -0,0 +1,10 @@
+package be.vdab.welkom.repositories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LandRepository {
+    public LandRepository() {
+        System.out.println("LandRepository constructor");
+    }
+}
