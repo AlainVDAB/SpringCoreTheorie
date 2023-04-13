47378c7c9637f95bd665c84d27b94d332b892ad2
Alain <alain.dossche@vdabcampus.be>
2023-04-13 11:39:42 +0200

    cursus p19 start oef application.properties

--- src/main/java/be/vdab/welkom/repositories/CsvTaalRepository.java
+++ src/main/java/be/vdab/welkom/repositories/CsvTaalRepository.java
@@ -3,6 +3,7 @@ package be.vdab.welkom.repositories;
 import be.vdab.welkom.domain.Taal;
 import be.vdab.welkom.exceptions.RepositoryException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,10 +13,16 @@ import java.util.List;
 
 @Component
 public class CsvTaalRepository implements TaalRepository {
+    private final String pad;
+
+    public CsvTaalRepository(@Value("${talenCsvPad}") String pad) {
+        this.pad = pad;
+    }
+
     @Override
     @Qualifier("CSV")
     public List<Taal> findAll(){
-        try(var stream = Files.lines(Path.of("/data/talen.csv"))){
+        try(var stream = Files.lines(Path.of(pad))){
             return stream
                     .map(regel -> regel.split(",")) // regel opsplitsen in zijn onderdelen
                     .map(regelOnderdelen -> // Taal object maken met die onderdelen
